36b2066dd89c7c42da064d6ede7bcba12d73156b
S David <2100425+s-daveb@users.noreply.github.com>
2024-05-16 19:26:48 -0400

    Disable unit tests in default config if this repository is a submodule of another

--- DefaultConfig.cmake
+++ DefaultConfig.cmake
@@ -6,7 +6,12 @@ include(Util)
 
 git_setup_submodules()
 prevent_in_source_build()
-disable_deprecated_Features()
+disable_deprecated_features()
 
+# When this package is included as a subproject, there's no need to
+# build and run the unit-tests.
+# Sets -DBUILD_TESTING to false by default if this is a third-party lib build
+# This check must appear beforezR project()
+disable_tests_if_subproject()
 
 # vim: ts=4 sts=4 sw=4 noet foldmethod=indent :
